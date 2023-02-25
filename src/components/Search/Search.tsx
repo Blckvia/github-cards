62552c5ec5bd984a8daa43f748cc19b4d88fe8da
blckvia <a.vyatkinn@icloud.com>
2023-02-25 11:19:02 +0500

    refused of useRef, only typification used

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,4 @@
 import { ReactComponent as SearchIcon } from 'assets/icon-search.svg';
-import { useRef } from 'react';
 import styles from './Search.module.scss';
 
 import { Button } from 'components/Button';
@@ -9,17 +8,20 @@ interface SearchProps {
   onSubmit: (text: string) => void;
 }
 
+type FormFields = {
+  username: HTMLInputElement;
+};
+
 export const Search = ({ hasError, onSubmit }: SearchProps) => {
-  const searchRef = useRef<HTMLInputElement | null>(null);
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement & FormFields>
+  ) => {
     event.preventDefault();
-
-    const text = searchRef.current ? searchRef.current.value : '';
+    const text = event.currentTarget.username.value;
 
     if (text) {
       onSubmit(text);
-      if (searchRef.current) searchRef.current.value = '';
+      event.currentTarget.reset();
     }
   };
 
@@ -30,7 +32,6 @@ export const Search = ({ hasError, onSubmit }: SearchProps) => {
           <SearchIcon />
         </label>
         <input
-          ref={searchRef}
           type='text'
           className={styles.textField}
           id='search'
